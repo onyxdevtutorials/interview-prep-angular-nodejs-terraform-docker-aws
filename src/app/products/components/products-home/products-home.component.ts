083b938b0146183e9f57f47bf6af62d902940aa4
David Silva <davidavilasilva@gmail.com>
2024-09-19 13:03:35 -0700

    Future location of a Products feature

---
+++ src/app/products/components/products-home/products-home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-products-home',
+  standalone: true,
+  imports: [],
+  templateUrl: './products-home.component.html',
+  styleUrl: './products-home.component.scss'
+})
+export class ProductsHomeComponent {
+
+}
