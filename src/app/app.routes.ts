edec2bd3167ec8c9f0d97e9f5e815a5c9fee15c2
David Silva <davidavilasilva@gmail.com>
2024-09-19 13:01:43 -0700

    Begin setting up the app, which I started on Stackblitz

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,19 @@
 import { Routes } from '@angular/router';
+import { ProductsHomeComponent } from './products/components/products-home/products-home.component';
+import { UsersHomeComponent } from './users/components/users-home/users-home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    redirectTo: '/users',
+    pathMatch: 'full',
+  },
+  {
+    path: 'products',
+    component: ProductsHomeComponent,
+  },
+  {
+    path: 'users',
+    component: UsersHomeComponent,
+  },
+];
