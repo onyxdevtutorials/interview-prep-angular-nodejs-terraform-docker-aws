b2d35e6c2910065edccee816e8f40552d92c97f3
David Silva <davidavilasilva@gmail.com>
2024-09-19 16:06:40 -0700

    Reorganize things to use child routes and a router outlet in the users-home component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,9 @@
 import { Routes } from '@angular/router';
 import { ProductsHomeComponent } from './products/components/products-home/products-home.component';
 import { UsersHomeComponent } from './users/components/users-home/users-home.component';
+import { UsersUpdateComponent } from './users/components/users-update/users-update.component';
+import { UsersCreateComponent } from './users/components/users-create/users-create.component';
+import { UsersListComponent } from './users/components/users-list/users-list.component';
 
 export const routes: Routes = [
   {
@@ -15,5 +18,19 @@ export const routes: Routes = [
   {
     path: 'users',
     component: UsersHomeComponent,
+    children: [
+      {
+        path: '',
+        component: UsersListComponent,
+      },
+      {
+        path: 'new',
+        component: UsersCreateComponent,
+      },
+      {
+        path: 'update/:userId',
+        component: UsersUpdateComponent,
+      },
+    ],
   },
 ];
--- src/app/users/components/users-home/users-home.component.ts
+++ src/app/users/components/users-home/users-home.component.ts
@@ -1,32 +1,19 @@
-import {
-  ChangeDetectionStrategy,
-  Component,
-  OnInit,
-  inject,
-} from '@angular/core';
-import { UsersService } from '../../services/users.service';
+import { Component } from '@angular/core';
 import { UsersListComponent } from '../users-list/users-list.component';
 import { UsersCreateComponent } from '../users-create/users-create.component';
-import { Observable, of } from 'rxjs';
-import { User } from '../../models/user.model';
 import { CommonModule } from '@angular/common';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-users-home',
   standalone: true,
-  imports: [CommonModule, UsersListComponent, UsersCreateComponent],
+  imports: [
+    CommonModule,
+    UsersListComponent,
+    RouterOutlet,
+    UsersCreateComponent,
+  ],
   templateUrl: './users-home.component.html',
   styleUrl: './users-home.component.scss',
-  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UsersHomeComponent implements OnInit {
-  usersService = inject(UsersService);
-
-  users$: Observable<User[]> = of([]);
-
-  bUsersFilter = (user: User) => user.lastName.startsWith('B');
-
-  ngOnInit(): void {
-    this.users$ = this.usersService.getUsers();
-  }
-}
+export class UsersHomeComponent {}
