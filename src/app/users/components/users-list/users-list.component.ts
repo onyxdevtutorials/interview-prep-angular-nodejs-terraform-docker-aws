cd26417dc3ccab7bcead39962fde1bfc047e50e2
David Silva <davidavilasilva@gmail.com>
2024-09-19 16:06:03 -0700

    Make UsersListComponent get users from the UsersService

--- src/app/users/components/users-list/users-list.component.ts
+++ src/app/users/components/users-list/users-list.component.ts
@@ -1,20 +1,38 @@
-import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
 import { User } from '../../models/user.model';
 import { MatCardModule } from '@angular/material/card';
+import { UsersService } from '../../services/users.service';
+import { Observable, of } from 'rxjs';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-users-list',
   standalone: true,
-  imports: [MatCardModule],
+  imports: [MatCardModule, CommonModule],
   templateUrl: './users-list.component.html',
   styleUrl: './users-list.component.scss',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class UsersListComponent {
-  @Input() users: User[] = [];
-  @Input() filterCriteria: (user: User) => boolean = () => true;
+export class UsersListComponent implements OnInit {
+  usersService = inject(UsersService);
 
-  get filteredUsers(): User[] {
-    return this.users.filter(this.filterCriteria);
+  users$: Observable<User[]> = of([]);
+
+  // bUsersFilter = (user: User) => user.lastName.startsWith('B');
+
+  ngOnInit(): void {
+    this.users$ = this.usersService.getUsers();
   }
+
+  // @Input() users: User[] = [];
+  // @Input() filterCriteria: (user: User) => boolean = () => true;
+
+  // get filteredUsers(): User[] {
+  //   return this.users.filter(this.filterCriteria);
+  // }
 }
