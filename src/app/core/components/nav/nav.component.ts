961e6aa1b7886606e13d15d9deb9ff0a6a3abe4b
David Silva <davidavilasilva@gmail.com>
2024-10-05 12:51:47 -0700

    Use Material Menu component for Users dropdown, provding navbar links to list and create a user

--- src/app/core/components/nav/nav.component.ts
+++ src/app/core/components/nav/nav.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatMenuModule } from '@angular/material/menu';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   standalone: true,
-  imports: [MatToolbarModule, RouterLink, RouterLinkActive],
+  imports: [MatToolbarModule, RouterLink, RouterLinkActive, MatMenuModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.scss',
 })
