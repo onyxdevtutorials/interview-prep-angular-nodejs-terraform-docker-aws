2ecceaecb0bb1872bf934af9ac4e51d4ebc48321
David Silva <davidavilasilva@gmail.com>
2024-10-22 11:48:55 -0700

    No longer need this

--- backend/src/setupModuleAlias.ts
+++
@@ -1,7 +0,0 @@
-import moduleAlias from 'module-alias';
-import path from 'path';
-
-const rootPath = path.resolve(__dirname, '..');
-moduleAlias.addAliases({
-  '@shared': path.join(rootPath, '../shared'),
-});
