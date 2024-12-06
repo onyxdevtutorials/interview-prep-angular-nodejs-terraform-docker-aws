e0d5e4566765325f85e3fd7507367d5662cbcd7c
David Silva <davidavilasilva@gmail.com>
2024-12-06 20:08:53 +0000

    Currently, images aren't being built into ECR, so of course it wouldn't be possible to pull them into ECS.

--- terraform/modules/iam/main.tf
+++ terraform/modules/iam/main.tf
@@ -22,3 +22,8 @@ resource "aws_iam_role_policy_attachment" "ecs_task_execution_role_policy" {
     role       = aws_iam_role.ecs_task_execution_role.name
     policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonECSTaskExecutionRolePolicy"
 }
+
+resource "aws_iam_role_policy_attachment" "ecr_pull_policy" {
+    role       = aws_iam_role.ecs_task_execution_role.name
+    policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+}
