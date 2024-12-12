0b855367831cc2aecb9f6547617bb67d177eeffb
David Silva <davidavilasilva@gmail.com>
2024-12-12 23:12:39 +0000

    Getting (all of a sudden) errors when running backend tests in the ci.yml workflow. The error is: `error: create table "knex_migrations" ("id" serial primary key, "name" varchar(255), "batch" integer, "migration_time" timestamptz) - duplicate key value violates unique constraint "pg_class_relname_nsp_index"`.

--- backend/migrations/20241027164858_create_users_table.ts
+++ backend/migrations/20241027164858_create_users_table.ts
@@ -2,21 +2,24 @@ import { Knex } from 'knex';
 import { UserStatus } from '@onyxdevtutorials/interview-prep-shared';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('users', (table) => {
-    table.increments('id').primary();
-    table.string('email').notNullable();
-    table.string('first_name').notNullable();
-    table.string('last_name').notNullable();
-    table
-      .enum('status', [
-        UserStatus.ACTIVE,
-        UserStatus.INACTIVE,
-        UserStatus.PENDING,
-      ])
-      .notNullable();
-  });
+  const exists = await knex.schema.hasTable('users');
+  if (!exists) {
+    return knex.schema.createTable('users', (table) => {
+      table.increments('id').primary();
+      table.string('email').notNullable();
+      table.string('first_name').notNullable();
+      table.string('last_name').notNullable();
+      table
+        .enum('status', [
+          UserStatus.ACTIVE,
+          UserStatus.INACTIVE,
+          UserStatus.PENDING,
+        ])
+        .notNullable();
+    });
+  }
 }
 
 export async function down(knex: Knex): Promise<void> {
-  return knex.schema.dropTable('users');
+  return knex.schema.dropTableIfExists('users');
 }
