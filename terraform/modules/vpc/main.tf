f126c613be168a92a9cc82b6b4d888fbd88987d4
David Silva <davidavilasilva@gmail.com>
2024-12-16 18:59:21 +0000

    Now that dns hostnames is enabled maybe this will work

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -1,5 +1,7 @@
 resource "aws_vpc" "interview_prep_vpc" {
     cidr_block = var.vpc_cidr
+    enable_dns_support = true
+    enable_dns_hostnames = true
     tags = {
         Name = "interview-prep-vpc"
         Environment = var.environment
