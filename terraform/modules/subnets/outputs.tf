0938fa4e5356bac08d7ad8cfb505492674642b5a
David Silva <davidavilasilva@gmail.com>
2024-12-20 22:14:45 +0000

    Create outputs for the new subnet ids

--- terraform/modules/subnets/outputs.tf
+++ terraform/modules/subnets/outputs.tf
@@ -3,9 +3,19 @@ output "subnet_a_id" {
   value       = aws_subnet.subnet_a.id
 }
 
+output "public_subnet_a_id" {
+  description = "The ID of public subnet A"
+  value       = aws_subnet.public_subnet_a.id
+}
+
 output "subnet_b_id" {
   description = "The ID of subnet B"
   value       = aws_subnet.subnet_b.id
+}
+
+output "public_subnet_b_id" {
+  description = "The ID of public subnet B"
+  value       = aws_subnet.public_subnet_b.id
 }
 
 output "db_subnet_a_id" {
@@ -13,9 +23,19 @@ output "db_subnet_a_id" {
   value       = aws_subnet.db_subnet_a.id
 }
 
+output "private_subnet_a_id" {
+  description = "The ID of private subnet A"
+  value       = aws_subnet.private_subnet_a.id
+}
+
 output "db_subnet_b_id" {
   description = "The ID of DB subnet B"
   value       = aws_subnet.db_subnet_b.id
+}
+
+output "private_subnet_b_id" {
+  description = "The ID of private subnet B"
+  value       = aws_subnet.private_subnet_b.id
 }
 
 output "nat_gateway_id" {
