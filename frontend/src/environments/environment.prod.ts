8a44ac4bd5d4d213d3c8a18be8dfaf4cadd5204f
David Silva <davidavilasilva@gmail.com>
2024-12-29 19:41:40 +0000

    I don't know how I came up with that url.... I thought it was supposed to be just "backend" (using a private dns)

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,8 @@
+// export const environment = {
+//     production: true,
+//     apiBaseUrl: 'http://backend.development.interview-prep.local:3000/api/v0',
+//     };
 export const environment = {
     production: true,
-    apiBaseUrl: 'http://backend.development.interview-prep.local:3000/api/v0',
-    };
+    apiBaseUrl: 'http://backend:3000/api/v0',
+};
