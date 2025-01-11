357d20d4276daac91b405cb45387d3fad60ac554
David Silva <davidavilasilva@gmail.com>
2025-01-11 21:21:03 +0000

    Try to make the builds and configuration names clearer

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,8 +1,4 @@
 export const environment = {
     production: true,
-    apiBaseUrl: 'https://api.dev.interviewprep.onyxdevtutorials.com/api/v0',
+    apiBaseUrl: 'https://api.prod.interviewprep.onyxdevtutorials.com/api/v0',
 };
-// export const environment = {
-//     production: true,
-//     apiBaseUrl: 'http://backend:3000/api/v0',
-// };
