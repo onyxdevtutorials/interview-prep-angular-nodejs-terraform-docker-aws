d23b0be8d673436046b0193bb4f692e12591e018
David Silva <davidavilasilva@gmail.com>
2025-01-17 02:51:32 +0000

    We *do* need the nat gateway -- for instance so that the migrate lambda function can get ssm params

--- terraform/modules/subnets/outputs.tf
+++ terraform/modules/subnets/outputs.tf
@@ -17,13 +17,3 @@ output "private_subnet_b_id" {
   description = "The ID of private subnet B"
   value       = aws_subnet.private_subnet_b.id
 }
-
-# output "nat_gateway_id" {
-#   description = "The ID of the NAT Gateway"
-#   value       = aws_nat_gateway.nat_gw.id
-# }
-
-# output "nat_eip_id" {
-#   description = "The ID of the NAT Gateway Elastic IP"
-#   value       = aws_eip.nat_eip.id
-# }
