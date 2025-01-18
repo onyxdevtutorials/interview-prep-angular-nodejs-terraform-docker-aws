2889a8e0523c6403e56b7eb4f72aef65eea8f083
David Silva <davidavilasilva@gmail.com>
2025-01-18 01:10:19 +0000

    Looks like I made a simple mistake.

--- backend/src/routes/health.ts
+++ backend/src/routes/health.ts
@@ -2,7 +2,7 @@ import { Router } from 'express';
 
 const healthRouter = Router();
 
-healthRouter.get('/health', (req, res) => {
+healthRouter.get('/', (req, res) => {
   res.status(200).send('OK');
 });
 
